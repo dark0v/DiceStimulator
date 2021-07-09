8e23c2b89e1a2f55173f274ac8818a338df3c821
Francisco <franciscojosegiaquinta@gmail.com>
2021-07-09 01:45:09 -0300

    added loop algorithm. Finished.

--- dicestimulator.py
+++ dicestimulator.py
@@ -1,47 +1,56 @@
 import random
 
-number = random.randint(1,6)
+print("This is a Dice Stimulator")
 
-if number == 1:
-    print("------------")
-    print("|          |")
-    print("|    0     |")
-    print("|          |")
-    print("------------")
+x = "y"
+
+while x == "y":
     
-if number == 2:
-    print("------------")
-    print("|          |")
-    print("|  0    0  |")
-    print("|          |")
-    print("------------")
+    number = random.randint(1,6)
 
-if number == 3:
-    print("------------")
-    print("|    0     |")
-    print("|    0     |")
-    print("|    0     |")
-    print("------------")
+    if number == 1:
+        print("------------")
+        print("|          |")
+        print("|    0     |")
+        print("|          |")
+        print("------------")
+        
+    if number == 2:
+        print("------------")
+        print("|          |")
+        print("|  0    0  |")
+        print("|          |")
+        print("------------")
 
-if number == 4:
-    print("------------")
-    print("|  0    0  |")
-    print("|          |")
-    print("|  0    0  |")
-    print("------------")
+    if number == 3:
+        print("------------")
+        print("|    0     |")
+        print("|    0     |")
+        print("|    0     |")
+        print("------------")
 
-if number == 5:
-    print("------------")
-    print("|  0    0  |")
-    print("|     0    |")
-    print("|  0    0  |")
-    print("------------")
+    if number == 4:
+        print("------------")
+        print("|  0    0  |")
+        print("|          |")
+        print("|  0    0  |")
+        print("------------")
 
-if number == 6:
-    print("------------")
-    print("|  0    0  |")
-    print("|  0    0  |")
-    print("|  0    0  |")
-    print("------------")
+    if number == 5:
+        print("------------")
+        print("|  0    0  |")
+        print("|     0    |")
+        print("|  0    0  |")
+        print("------------")
+
+    if number == 6:
+        print("------------")
+        print("|  0    0  |")
+        print("|  0    0  |")
+        print("|  0    0  |")
+        print("------------")
+        
+    x = input("Enter Y to roll again")
+
 
 
