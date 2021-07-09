05950ea9d576edce04a3805539af2d6e76b02ff5
Francisco <franciscojosegiaquinta@gmail.com>
2021-07-09 01:35:54 -0300

    added dice graphic representation. still under development

--- dicestimulator.py
+++ dicestimulator.py
@@ -1,5 +1,47 @@
 import random
 
-x = random.randint(1,6)
+number = random.randint(1,6)
 
-print(x)
+if number == 1:
+    print("------------")
+    print("|          |")
+    print("|    0     |")
+    print("|          |")
+    print("------------")
+    
+if number == 2:
+    print("------------")
+    print("|          |")
+    print("|  0    0  |")
+    print("|          |")
+    print("------------")
+
+if number == 3:
+    print("------------")
+    print("|    0     |")
+    print("|    0     |")
+    print("|    0     |")
+    print("------------")
+
+if number == 4:
+    print("------------")
+    print("|  0    0  |")
+    print("|          |")
+    print("|  0    0  |")
+    print("------------")
+
+if number == 5:
+    print("------------")
+    print("|  0    0  |")
+    print("|     0    |")
+    print("|  0    0  |")
+    print("------------")
+
+if number == 6:
+    print("------------")
+    print("|  0    0  |")
+    print("|  0    0  |")
+    print("|  0    0  |")
+    print("------------")
+
+
